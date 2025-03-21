28d66235f51d00047dbd6121109ac4a1986062e8
UrielBatista <ur.sabatista@gmail.com>
2025-03-21 18:56:33 -0300

    updated request

--- src/services/getPokemon.js
+++ src/services/getPokemon.js
@@ -2,7 +2,7 @@ import api from './api';
 
 // GET ALL POKEMON
 export async function GetAllPokemons() {
-    const response = await api.get(`/?limit=1000`)
+    const response = await api.get(`/?limit=100000&offset=0`)
     return response.data;
 }
 // GET POKEMON 
